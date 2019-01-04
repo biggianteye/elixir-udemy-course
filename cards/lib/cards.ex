5a5fb986745b776aceeffb20f75e440a3b26ec43
Burhan Ali <burhan.ali@graze.com>
2019-01-04 22:11:58 +0000

    Add some card functions

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -4,6 +4,23 @@ defmodule Cards do
   """
 
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = [
+      "A", "2", "3", "4", "5", "6", "7",
+      "8", "9", "10", "J", "Q", "K"
+    ]
+
+    suits = ["♣", "♦", "♥", "♠"]
+
+    for value <- values, suit <- suits do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
