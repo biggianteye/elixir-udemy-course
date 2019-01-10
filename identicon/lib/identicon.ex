869b491b6383b93ad84f4b52ff73d50710dbad88
Burhan Ali <burhan.ali@graze.com>
2019-01-10 22:42:51 +0000

    Add some function docs

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -4,6 +4,14 @@ defmodule Identicon do
     |> hash_input
   end
 
+  @doc """
+  Produces an MD5 hash of the given input as a list of bytes.
+
+  ## Examples
+
+      iex> Identicon.hash_input("Hello world!")
+      [134, 251, 38, 157, 25, 13, 44, 133, 246, 224, 70, 140, 236, 164, 42, 32]
+  """
   def hash_input(input) do
     :crypto.hash(:md5, input)
     |> :binary.bin_to_list
